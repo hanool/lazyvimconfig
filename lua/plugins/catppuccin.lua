8f4a1981d52644e6bab6fcd473d8a9e143b13051
hanool <29415530+hanool@users.noreply.github.com>
2025-01-06 13:13:06 +0900

    update: change colorscheme

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      flavour = "mocha", -- latte, frappe, macchiato, mocha
+      transparent = true,
+    },
+  },
+}
