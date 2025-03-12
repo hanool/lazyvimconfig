7543a59c02e120889036eed77117596eb58d752f
hanool <29415530+hanool@users.noreply.github.com>
2025-03-12 09:55:44 +0900

    add: fold.scm for rust

---
+++ queries/rust/fold.scm
@@ -0,0 +1,19 @@
+[
+  (mod_item)
+  (foreign_mod_item)
+  (function_item)
+  (struct_item)
+  (trait_item)
+  (enum_item)
+  (impl_item)
+  (type_item)
+  (union_item)
+  (const_item)
+  (match_expression)
+  (macro_definition)
+  (attribute_item)
+  (block)
+  (use_declaration)*
+] @fold
+
+((line_comment)+ @fold)
