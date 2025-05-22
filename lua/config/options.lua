3d990e0e18a4ec65413e923d679580f3974501df
hanool <29415530+hanool@users.noreply.github.com>
2025-05-22 15:31:39 +0900

    add: '<leader>vx' for vue upgrade macro

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -28,3 +28,69 @@ if is_wsl then
     cache_enabled = 0,
   }
 end
+
+local function upgrade_vue()
+  local filepath = vim.fn.expand("%:p")
+  local cmd = { "npx", "vue-upgrade-tool", "--files", filepath }
+
+  vim.fn.jobstart(cmd, {
+    stdout_buffered = true,
+    stderr_buffered = true,
+    on_stdout = function(_, data)
+      if data then
+        vim.notify(table.concat(data, "\n"), vim.log.levels.INFO)
+      end
+    end,
+    on_stderr = function(_, data)
+      if data then
+        vim.notify(table.concat(data, "\n"), vim.log.levels.ERROR)
+      end
+    end,
+    on_exit = function(_, code)
+      vim.schedule(function()
+        vim.cmd("checktime")
+        if code == 0 then
+          vim.notify("upgrade vue successfully", vim.log.levels.INFO)
+        else
+          vim.notify("upgrade vue failed (exit code " .. code .. ")", vim.log.levels.ERROR)
+        end
+      end)
+    end,
+  })
+end
+
+local function transform_vue()
+  local filepath = vim.fn.expand("%:p")
+  local cmd = { "npx", "scriptshifter", "--files", filepath, "--vue", "2.7" }
+
+  vim.fn.jobstart(cmd, {
+    stdout_buffered = true,
+    stderr_buffered = true,
+    on_stdout = function(_, data)
+      if data then
+        vim.notify(table.concat(data, "\n"), vim.log.levels.INFO)
+      end
+    end,
+    on_stderr = function(_, data)
+      if data then
+        vim.notify(table.concat(data, "\n"), vim.log.levels.ERROR)
+      end
+    end,
+    on_exit = function(_, code)
+      vim.schedule(function()
+        vim.cmd("checktime")
+        if code == 0 then
+          vim.notify("converted options api to composition api successfully", vim.log.levels.INFO)
+          upgrade_vue()
+        else
+          vim.notify(
+            "converting options api to composition api failed (exit code " .. code .. ")",
+            vim.log.levels.ERROR
+          )
+        end
+      end)
+    end,
+  })
+end
+
+vim.keymap.set("n", "<leader>vx", transform_vue, { noremap = true, silent = true })
